af958ddff7232014cfb6fd51c51024179722081a
Francesco <francesco.mistri98@gmail.com>
2023-04-13 01:35:03 +0200

    Simple playable CLI game
    No sanity check for correct actions. It is possible to cheat.

--- corso/cli.py
+++ corso/cli.py
@@ -1,4 +1,4 @@
-from corso.model import Corso, Board
+from corso.model import Corso, Board, Action
 
 MARBLES = ['O', 'A', 'B']
 CELLS = ['O', 'a', 'b']
@@ -19,12 +19,43 @@ def print_board(board: Board):
     print('\n'.join(map(' '.join, output_board)))
 
 
+def get_input(state: Corso) -> tuple[int, int]:
+    """Retrieve row, column input from user."""
+    while True:
+        input_string = input()
+        values = input_string.split()
+
+        if len(values) != 2:
+            print('Insert two values separated by a whitespace')
+            continue
+
+        integers_ok = True
+        try:
+            row, col = map(int, values)
+            integers_ok &= 0 <= row < state.height
+            integers_ok &= 0 <= col < state.width
+        except ValueError:
+            integers_ok = False
+
+        if not integers_ok:
+            print('Input values must be integers.\n'
+                  f'The first value must be in the range [0, {state.height}). '
+                  f'The second value must be in the range [0, {state.width}).')
+            continue
+
+        return row, col
+
+
 def cli_game():
     state = Corso()
 
     while not state.terminal[0]:
+        print('Player', state.player_index)
         print_board(state.board)
-        input()
+        row, col = get_input(state)
+        state = state.step(Action(state.player_index, row, col))
+
+    print(state.terminal)
 
 
 if __name__ == '__main__':
